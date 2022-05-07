9015d20b4c41f659b4679efafac491a4447bbbb5
Denise <denisemarielf@gmail.com>
2022-05-07 00:48:14 -0300

    Add features: toggle metric units, error message, display correct weather img

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import { fromUnixTime, format, locale} from 'date-fns'
 
-
-
 const cityInput =  document.getElementById('city-input');
 const form = document.querySelector('form');
 const unitsOfMeasurement = document.querySelectorAll(".unit-of-measurement")
-
-
-//imperial
-
+const lenghtUnit = document.getElementById('lenght-unit')
 const buttonsUnit = document.querySelectorAll("#metric,#imperial")
 
 buttonsUnit.forEach((button => {
@@ -16,11 +11,13 @@ buttonsUnit.forEach((button => {
         if (button.getAttribute('id') === 'metric') {
             info.unit = 'metric';
             getWeather(info.cityInput, info.unit)
-            setUnitOfMeasurement('°C')
+            button.setAttribute('class', 'selected')
+            buttonsUnit[1].removeAttribute('class')
         } else {
             info.unit = 'imperial'
-            getWeather(info.cityInput, info.unit)
-            setUnitOfMeasurement('°F')
+            getWeather(info.cityInput, info.unit);
+            button.setAttribute('class', 'selected')
+            buttonsUnit[0].removeAttribute('class')
         }
     })
 }))
@@ -42,25 +39,25 @@ async function getWeather(city, unit) {
         const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=' + unit + '&APPID=bf4889f5db8bf85d0f2f84f48098efff', {mode: 'cors'})
         const weatherData = await response.json();
             info.city = weatherData.name,
+            info.country = weatherData.sys.country,
             info.main = weatherData.weather[0].main,
             info.description = weatherData.weather[0].description,
             info.temp = Math.floor(weatherData.main.temp),
-            info.tempMax= Math.floor(weatherData.main.temp_max),
-            info.tempMin = Math.floor(weatherData.main.temp_min),
+            info.tempMax= "MAX " + Math.floor(weatherData.main.temp_max),
+            info.tempMin = "MIN " + Math.floor(weatherData.main.temp_min),
             info.feelsLike = Math.floor(weatherData.main.feels_like),
-            info.humidity = weatherData.main.humidity,
-            info.pressure = weatherData.main.pressure,
-            info.sunrise = 'Sunrise '+format(fromUnixTime(weatherData.sys.sunrise), "p"),
-            info.sunset = 'Sunset '+format(fromUnixTime(weatherData.sys.sunset), "p"),
-            info.wind = Math.floor(weatherData.wind.speed);
-            info.date = new Date().toDateString()
+            info.humidity = weatherData.main.humidity + " %",
+            info.pressure = weatherData.main.pressure + " mb",
+            info.sunrise = format(fromUnixTime(weatherData.sys.sunrise), "p"),
+            info.sunset = format(fromUnixTime(weatherData.sys.sunset), "p"),
+            info.wind = weatherData.wind.speed;
+            info.date = format(new Date(), "iiii', 'HH:mm")
             info.visibility = weatherData.visibility;
         displayWeather()
-        console.log(weatherData)
         return weatherData;
     }
     catch (error){
-        console.log(error)
+        document.querySelector('small').style.display = 'flex';
     }
 }
 
@@ -70,7 +67,8 @@ function displayWeather(){
     document.getElementById('temp').innerHTML=info.temp;
     document.getElementById('date').innerHTML=info.date;
     document.getElementById('main').innerHTML=info.main;
-    document.querySelector('#city-container>p').innerHTML=info.city;
+    document.getElementById('description').innerHTML=info.description;
+    document.querySelector('#city-container>p').innerHTML=info.city+", "+info.country;
     document.getElementById('max-temp').innerHTML=info.tempMax;
     document.getElementById('min-temp').innerHTML=info.tempMin;
     document.getElementById('wind').innerHTML=info.wind;
@@ -79,6 +77,16 @@ function displayWeather(){
     document.getElementById('sunrise').innerHTML=info.sunrise;
     document.getElementById('sunset').innerHTML=info.sunset;
     document.getElementById('pressure').innerHTML=info.pressure;
+    document.querySelector('img').src="images/"+info.main+".png";
+    document.querySelector('small').style.display = 'none';
+    if(info.unit === 'metric') {
+        setUnitOfMeasurement('°C')
+        lenghtUnit.innerHTML = 'km/h'
+    }else{
+        setUnitOfMeasurement('°F')
+            lenghtUnit.innerHTML = "mph"
+    }
+    
 }
 
 
@@ -86,5 +94,7 @@ form.addEventListener('submit', function(e){
     e.preventDefault()
     info.cityInput = cityInput.value;
     getWeather(info.cityInput, info.unit)
+    form.reset();
 })
 
+ 
